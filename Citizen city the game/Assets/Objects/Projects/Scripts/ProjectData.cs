002966b4c3f543c6bf2ca150be6e68cf075049d4
LeviOS31 <levi.oldescheper@gmail.com>
2026-03-31 09:57:38 +0200

    GetProject functions done

--- Citizen city the game/Assets/Objects/Projects/Scripts/ProjectData.cs
+++ Citizen city the game/Assets/Objects/Projects/Scripts/ProjectData.cs
@@ -13,10 +13,23 @@ public class ProjectData : ScriptableObject
     public string ProjectName;
     [Tooltip("Personal: all data from the same player used in project \n Group: data from different municipalities needed for project completion \n Open: for the multiplier project")]
     public ProjectType ProjectType;
-    public Sprite ProjectIcon;
+    [Tooltip("Image not required")]
+    public Sprite ProjectImage;
     public string ProjectDescription;
     public List<DataRequired> NeededData;
     public int ScoreValue;
+
+    public bool HasRequiredColor( Color targetColor)
+    {
+        foreach (DataRequired requirement in NeededData)
+        {
+            if (requirement.Color == targetColor)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 public class DataRequired
