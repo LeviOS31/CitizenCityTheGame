94bb244ba1d835497df9670f337e042017628d7d
LeviOS31 <58821386+LeviOS31@users.noreply.github.com>
2026-05-13 12:02:37 +0200

    added some more accessibility settings

--- Citizen city the game/Assets/Objects/StartMenu/Scripts/Accessibility.cs
+++ Citizen city the game/Assets/Objects/StartMenu/Scripts/Accessibility.cs
@@ -6,8 +6,13 @@ public class Accessibility : MonoBehaviour
     public TMP_FontAsset NormalFont;
     public TMP_FontAsset HighContrastFont;
 
+    public TMP_FontAsset FontTNRoman;
+    public TMP_FontAsset FontComicSans;
+    public TMP_FontAsset FontOpenSans;
+
     private void Awake()
     {
+        PlayerPrefs.DeleteAll();
         DontDestroyOnLoad(this.gameObject);
     }
 
@@ -27,10 +32,39 @@ public class Accessibility : MonoBehaviour
             {
                 text.color = new Color(0.85f, 1f, 0f);
                 text.font = HighContrastFont;
+            }else{
+                if (PlayerPrefs.GetInt("Font",0) == 0)
+                {
+                    memory.Reset();
+                }
+                else
+                {
+                    text.color = memory.NormalColor;
+                    switch (PlayerPrefs.GetInt("Font",0))
+                    {
+                        case 1:
+                            text.font = FontComicSans;
+                            break;
+                        case 2:
+                            text.font = FontOpenSans;
+                            break;
+                        case 3:
+                            text.font = FontTNRoman;
+                            break;
+                    }
+                }
             }
-            else
+
+            float fontsize = PlayerPrefs.GetInt("FontSizeOffset", 0);
+
+            text.fontSize = memory.NormalFontSize * (1f + fontsize / 100f);
+
+            if (fontsize != 0)
             {
-                memory.Reset();
+                Debug.Log(memory.NormalFontSize + " * " + "(1 + " + fontsize + " / 100)");
+                Debug.Log("=");
+                Debug.Log(memory.NormalFontSize * (1f + fontsize / 100f));
+                Debug.Log(" ");
             }
         }
     }
--- Citizen city the game/Assets/Objects/StartMenu/Scripts/AccessibilitySettings.cs
+++ Citizen city the game/Assets/Objects/StartMenu/Scripts/AccessibilitySettings.cs
@@ -1,10 +1,12 @@
+using TMPro;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class AccessibilitySettings : MonoBehaviour
 {
     public Toggle toggleHighContrast;
+    public Slider FontSizeOffset;
+    public TMP_Dropdown Fontselector;
 
     private void Start()
     {
@@ -16,6 +18,9 @@ public class AccessibilitySettings : MonoBehaviour
         {
             toggleHighContrast.isOn = false;
         }
+
+        FontSizeOffset.value = PlayerPrefs.GetInt("FontSizeOffset", 0);
+        
     }
     public void toggle()
     {
@@ -27,5 +32,15 @@ public class AccessibilitySettings : MonoBehaviour
         {
             PlayerPrefs.SetInt("HighContrast", 0);
         }
+    }
+
+    public void changeFontSize()
+    {
+        PlayerPrefs.SetInt("FontSizeOffset", (int)FontSizeOffset.value);
+    }
+
+    public void ChangeFont()
+    {
+        PlayerPrefs.SetInt("Font", Fontselector.value);
     }
 }
